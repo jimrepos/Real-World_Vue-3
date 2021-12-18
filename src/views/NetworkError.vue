7a7e973d563e48eeddb6286356153e605c08dc46
James Mountford <james@mountford.me>
2021-12-18 23:44:29 +0000

    Added NetworkError view.

---
+++ src/views/NetworkError.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="networkError">
+        <h1>Uh-Oh!</h1>
+        <p>
+            Looks like you're having some network issues, please <a href="#" @click="$router.go(-1)">click here</a> and try again.
+        </p>
+    </div>
+</template>
