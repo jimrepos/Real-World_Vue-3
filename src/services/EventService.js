fe6bc725ed563e29f001b2d93bb20f84954a502d
James Mountford <james@mountford.me>
2021-12-18 00:44:27 +0000

    Updated EventList to load items via axios from db.

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const apiClient = axios.create(
+    {
+        baseURL: 'https://my-json-server.typicode.com/jimrepos/Real-World_Vue-3',
+        withCredentials: false,
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+)
+
+export default {
+    getEvents() {
+        return apiClient.get('/events')
+    }
+}
