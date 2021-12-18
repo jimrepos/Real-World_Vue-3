597acf30cf22ec85594230ed89bc5889eb5cafbd
James Mountford <james@mountford.me>
2021-12-18 22:18:45 +0000

    Completed Programmatic Navigation task.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,12 +1,40 @@
 <template>
     <div v-if="event">
         <p>Register for the event here...</p>
+        <button @click="register">Submit Details</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'EventDetails',
-        props: ['event']
+        props: ['event'],
+        methods: {
+            register() {
+                // Redirect back to the event details page (should be once successfully submitted)
+                this.$router.push({
+                    name: 'EventDetails',
+                    params: {
+                        id: this.event.id
+                    }
+                });
+            }
+        }
     }
 </script>
+
+<style scoped>
+    button {
+        background: #42b983;
+        border: 1px solid #028953;
+        border-radius: 5px;
+        color: #fff;
+        font-size: 1.2em;
+        padding: 0.8em 1.5em;
+        margin: 2em 0;
+        cursor: pointer;
+        font-weight: bold;
+        text-shadow: 2px 2px 0px rgba(0,0,0,0.3);
+        box-shadow: 0px 8px 15px rgba(0,0,0,0.3);
+    }
+</style>
